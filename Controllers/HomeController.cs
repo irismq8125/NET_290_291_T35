2c422d6ba32ae3217483bfb25fd3c9320e919e26
irismq8125 <mauquy123@gmail.com>
2024-03-21 10:24:53 +0700

    init to do task

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using NET_290_291_T35.Models;
+using NET_290_291_T35.Models.ToDoTask;
 using System.Diagnostics;
 
 namespace NET_290_291_T35.Controllers
@@ -7,6 +8,7 @@ namespace NET_290_291_T35.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        public static List<ToDoTask> toDoTasks;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -15,7 +17,8 @@ namespace NET_290_291_T35.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            toDoTasks = new List<ToDoTask>();
+            return View(toDoTasks);
         }
     }
 }
