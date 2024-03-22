57440d7fe69a786077edec31fe931dfa1923a18d
irismq8125 <mauquy123@gmail.com>
2024-03-22 07:59:05 +0700

    update master

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,7 +6,6 @@ namespace NET_290_291_T35.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        public static List<ToDoTask> toDoTasks = new List<ToDoTask>();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -15,45 +14,7 @@ namespace NET_290_291_T35.Controllers
 
         public IActionResult Index()
         {
-            var items = toDoTasks.ToList();
-            return View(items);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> LoadData(string trangthai)
-        {
-            var items = trangthai == "0" ? toDoTasks.ToList() : toDoTasks.Where(x => x.Status == trangthai).ToList();
-            return Ok(items);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> AddToDoTask(string ten, string batdau, string ketthuc, string trangthai, string mota)
-        {
-            ToDoTask task = new ToDoTask
-            {
-                Id = Guid.NewGuid(),
-                TaskName = ten,
-                StartDay = batdau,
-                EndDay = ketthuc,
-                Status = trangthai,
-                Discription = mota
-            };
-
-            toDoTasks.Add(task);
-            return Ok(task);
-        }
-
-        public async Task<IActionResult> DetailToDoTask(string id)
-        {
-            var item = toDoTasks.FirstOrDefault(x => x.Id.ToString() == id);
-            return Ok(item);
-        }
-
-        public async Task<IActionResult> DeleteToDoTask(string id)
-        {
-            var item = toDoTasks.FirstOrDefault(x => x.Id.ToString() == id);
-            toDoTasks.Remove(item);
-            return Ok();
+            return View();
         }
     }
 }
